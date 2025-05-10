81e3d5f8fb0c0dd677ff744c3228f79d3a7f10bb
JSean0930 <122076254+JSean0930@users.noreply.github.com>
2025-05-10 11:18:31 +0800

    更新 long_mpc.py

--- selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
+++ selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
@@ -392,7 +392,7 @@ class LongitudinalMpc:
     stop_distance = get_STOP_DISTANCE(personality)
 
     if Params().get_bool("ToyotaTune") and not (self.CP.flags & ToyotaFlags.SMART_DSU):
-      stop_distance += 7.0
+      stop_distance += 3.0
 
     self.status = radarstate.leadOne.status or radarstate.leadTwo.status
 
