df6a347dc133a66baa75fe00000a9e2a1161b2ae
JSean0930 <122076254+JSean0930@users.noreply.github.com>
2025-05-16 06:56:29 +0800

    Update long_mpc.py

--- selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
+++ selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
@@ -417,9 +417,9 @@ class LongitudinalMpc:
 
       # Fake an obstacle for cruise, this ensures smooth acceleration to set speed
       # when the leads are no factor.
-      v_lower = v_ego + (T_IDXS * CRUISE_MIN_ACCEL * 0.9)
+      v_lower = v_ego + (T_IDXS * CRUISE_MIN_ACCEL * 0.95)
       # TODO does this make sense when max_a is negative?
-      v_upper = v_ego + (T_IDXS * CRUISE_MAX_ACCEL * 1.2)
+      v_upper = v_ego + (T_IDXS * CRUISE_MAX_ACCEL * 0.9)
       v_cruise_clipped = np.clip(v_cruise * np.ones(N+1),
                                  v_lower,
                                  v_upper)
@@ -436,7 +436,7 @@ class LongitudinalMpc:
       x_obstacles = np.column_stack([lead_0_obstacle,
                                      lead_1_obstacle])
       # cruise 目標距離（略為積極）
-      cruise_target = T_IDXS * np.clip(v_cruise * 1.0, v_ego - 4.0, 1e3) + x[0]
+      cruise_target = T_IDXS * np.clip(v_cruise * 1.0, v_ego - 2.0, 1e3) + x[0]
 
       # e2e 預測距離
       xforward = ((v[1:] + v[:-1]) / 2) * (T_IDXS[1:] - T_IDXS[:-1]) 
@@ -446,7 +446,7 @@ class LongitudinalMpc:
       x_and_cruise = np.column_stack([x * 0.95, cruise_target])
       #x = np.max(x_and_cruise, axis=1)
       #計算速度加權：低速偏 e2e，高速偏 cruise
-      w = np.clip((v_ego - 5.0) / 15.0, 0.0, 1.0)  #15
+      w = np.clip((v_ego - 5.0) / 10.0, 0.0, 1.0)  #15
       x = (1 - w) * np.min(x_and_cruise, axis=1) + w * np.max(x_and_cruise, axis=1)
       # 若 e2e 比 cruise 明顯遠，才使用 e2e 作為來源
       self.source = 'e2e' if x_and_cruise[1,0] > x_and_cruise[1,1] *1.1 else 'cruise'
