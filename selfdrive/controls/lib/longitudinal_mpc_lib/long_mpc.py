de397bc473d3a4d59c519e41ffd30e03fd8d0845
JSean0930 <122076254+JSean0930@users.noreply.github.com>
2025-05-19 13:08:17 +0800

    Update long_mpc.py

--- selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
+++ selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
@@ -405,9 +405,8 @@ class LongitudinalMpc:
     return lead_xv
 
 def update(self, radarstate, v_cruise, x, v, a, j, v_ego, v_lead, a_lead, personality=log.LongitudinalPersonality.standard, dynamic_follow=False):
-  t_follow = get_T_FOLLOW(v_ego, v_lead, a_lead, personality)
   v_ego = self.x0[1]
-  t_follow = get_T_FOLLOW(v_ego, v_lead, a_lead, personality) if not dynamic_follow else get_dynamic_follow(v_ego, personality)
+  t_follow = get_dynamic_follow(v_ego, personality) if dynamic_follow else get_T_FOLLOW(v_ego, v_lead, a_lead, personality)
   stop_distance = get_STOP_DISTANCE(personality)
 
   if Params().get_bool("ToyotaTune") and not (self.CP.flags & ToyotaFlags.SMART_DSU):
