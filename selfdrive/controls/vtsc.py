7b4e8715592f2983d9258f05a9136155d1206988
JSean0930 <122076254+JSean0930@users.noreply.github.com>
2025-05-21 07:49:58 +0800

    Update vtsc.py

--- selfdrive/controls/vtsc.py
+++ selfdrive/controls/vtsc.py
@@ -10,7 +10,7 @@ from openpilot.common.params import Params
 
 params = Params()
 
-TARGET_LAT_A = 2.3  # m/s^2
+TARGET_LAT_A = 2.6  # m/s^2 2.3
 MIN_TARGET_V = 5.0  # m/s
 HISTORY_LENGTH = 10  # number of past curvature samples to average
 SMOOTHING_ALPHA = 0.3  # for v_target exponential smoothing
