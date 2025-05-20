6c13ca9f120320332ea29df98d1489ec7e89ac5c
JSean0930 <122076254+JSean0930@users.noreply.github.com>
2025-05-20 20:19:16 +0800

    Update long_mpc.py

--- selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
+++ selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
@@ -106,7 +106,7 @@ def get_adaptive_T_FOLLOW(v_ego, a_lead, personality=log.LongitudinalPersonality
 
   # 2. 隨車速線性增加的時距
   #    0 m/s -> +0 s, 33 m/s -> +0.5 s
-  K_SPEED = 0.2 / 33.0     # 每 1 m/s 約增加 0.015 s
+  K_SPEED = 0.2 / 33.0     # 每 1 m/s 約增加 0.006 s
   extra_speed_t = np.clip(K_SPEED * v_ego, 0.0, 0.5)
   base_t_follow += extra_speed_t
   
@@ -462,7 +462,7 @@ class LongitudinalMpc:
       self.yref[:,3] = a
       self.yref[:,5] = j
       for i in range(N):
-      self.solver.set(i, "yref", self.yref[i])
+        self.solver.set(i, "yref", self.yref[i])
       self.solver.set(N, "yref", self.yref[N][:COST_E_DIM])
 
       e2e_dist = x_e2e[1]
