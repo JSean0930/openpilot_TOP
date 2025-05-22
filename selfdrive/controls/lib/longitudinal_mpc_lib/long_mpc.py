bd0e215ab1a1ad4b5e6376b47553adc176630210
JSean0930 <122076254+JSean0930@users.noreply.github.com>
2025-05-23 07:16:33 +0800

    Update long_mpc.py

--- selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
+++ selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
@@ -144,16 +144,16 @@ def get_stopped_equivalence_factor(v_lead, v_ego):
     v_diff_offset = np.maximum(v_diff_offset * ((speed_to_reach_max_v_diff_offset - v_ego)/speed_to_reach_max_v_diff_offset), 0)
   return (v_lead**2) / (2 * COMFORT_BRAKE) + v_diff_offset
 
-def get_safe_obstacle_distance(v_ego, t_follow, stop_distance=None):
+def get_safe_obstacle_distance(v_ego, t_follow, stop_distance=None, personality=log.LongitudinalPersonality.standard):
   if stop_distance is None:
-    stop_distance = get_STOP_DISTANCE()
+    stop_distance = get_STOP_DISTANCE(personality)
   return (v_ego**2) / (2 * COMFORT_BRAKE) + t_follow * v_ego + stop_distance
 
 def desired_follow_distance(v_ego, v_lead, a_lead, t_follow=None, stop_distance=None, personality=log.LongitudinalPersonality.standard):
   if t_follow is None:
     t_follow = get_adaptive_T_FOLLOW(v_ego, a_lead, personality)
   if stop_distance is None:
-    stop_distance = get_STOP_DISTANCE()
+    stop_distance = get_STOP_DISTANCE(personality)
   return get_safe_obstacle_distance(v_ego, t_follow, stop_distance) - get_stopped_equivalence_factor(v_lead, v_ego)
 
 
