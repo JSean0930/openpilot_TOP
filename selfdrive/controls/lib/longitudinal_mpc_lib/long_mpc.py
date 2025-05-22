9f703b8b6127e95f098129fe619f6c5fc0dd72e6
JSean0930 <122076254+JSean0930@users.noreply.github.com>
2025-05-22 23:26:24 +0800

    Update long_mpc.py

--- selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
+++ selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
@@ -149,7 +149,7 @@ def get_safe_obstacle_distance(v_ego, t_follow, stop_distance=None):
     stop_distance = get_STOP_DISTANCE()
   return (v_ego**2) / (2 * COMFORT_BRAKE) + t_follow * v_ego + stop_distance
 
-def desired_follow_distance(v_ego, v_lead, a_lead, t_follow=None, stop_distance=None):
+def desired_follow_distance(v_ego, v_lead, a_lead, t_follow=None, stop_distance=None, personality=log.LongitudinalPersonality.standard):
   if t_follow is None:
     t_follow = get_adaptive_T_FOLLOW(v_ego, a_lead, personality)
   if stop_distance is None:
