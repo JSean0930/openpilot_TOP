39fecef55307029e6d13dfc58f6fc1536d847cf7
JSean0930 <122076254+JSean0930@users.noreply.github.com>
2025-05-24 14:47:03 +0800

    更新 long_mpc.py

--- selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
+++ selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
@@ -120,7 +120,7 @@ def get_adaptive_T_FOLLOW(v_ego, a_lead, personality=log.LongitudinalPersonality
 
 def get_STOP_DISTANCE(personality=log.LongitudinalPersonality.standard):
   if personality==log.LongitudinalPersonality.relaxed:
-    return 8.0
+    return 9.0
   elif personality==log.LongitudinalPersonality.standard:
     return 9.0
   elif personality==log.LongitudinalPersonality.aggressive:
