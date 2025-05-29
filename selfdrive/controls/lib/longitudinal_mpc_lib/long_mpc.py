5da2c0de6d1cbd36dbeded67951b150fbfe7fdf5
JSean0930 <122076254+JSean0930@users.noreply.github.com>
2025-05-29 20:01:07 +0800

    Update long_mpc.py

--- selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
+++ selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
@@ -472,7 +472,7 @@ class LongitudinalMpc:
       x = (1 - w) * np.min(x_and_cruise, axis=1) + w * np.max(x_and_cruise, axis=1)
       #==========================================================================
       # 若 e2e 比 cruise 明顯遠，才使用 e2e 作為來源
-      if v_ego < 11.11:
+      if speed_kph < 50:
         self.source = 'e2e' if x_and_cruise[1,0] > x_and_cruise[1,1] else 'cruise' # 當 e2e 預測距離較 cruise 超前 10% 時，才真正採用 e2e 軌跡。這個閾值越低，越容易觸發 e2e 跟隨，其激進程度也越可能推高轉速。
       else:
         self.source = 'e2e' if x_and_cruise[1,0] < x_and_cruise[1,1] else 'cruise'
