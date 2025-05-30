39986d94dd0a476bf3183cab84bf67cb5702fb35
JSean0930 <122076254+JSean0930@users.noreply.github.com>
2025-05-30 19:14:26 +0800

    更新 long_mpc.py

--- selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
+++ selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
@@ -470,7 +470,7 @@ class LongitudinalMpc:
 
       # 混合 e2e 和 cruise，根據速度平滑插值
       x_and_cruise = np.column_stack([x * 1.0, cruise_target]) # 將 e2e 預測距離額外乘以 0.95，會讓 e2e 軌跡對加速目標略顯保守。數值越接近 1，e2e 的影響越大；越小，則更偏向 cruise，進而影響加速決策和引擎轉速。
-      x = np.max(x_and_cruise, axis=1)
+      #x = np.max(x_and_cruise, axis=1)
       #計算速度加權：低速偏 e2e，高速偏 cruise
       w = np.clip((v_ego - 5.0) / 28.0, 0.0, 0.2)  #15
       # 高速時再衰減
