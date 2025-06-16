490b7d0a6fda12d748f8d2b63aa316ac527c0b1a
JSean0930 <122076254+JSean0930@users.noreply.github.com>
2025-06-17 07:47:08 +0800

    Update long_mpc.py

--- selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
+++ selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
@@ -91,7 +91,7 @@ def get_jerk_factor(personality=log.LongitudinalPersonality.standard):
 
 def get_T_FOLLOW(personality=log.LongitudinalPersonality.standard):
   if personality==log.LongitudinalPersonality.relaxed:
-    return 2.5#1.45
+    return 2.0 #1.45
   elif personality==log.LongitudinalPersonality.standard:
     return 1.35
   elif personality==log.LongitudinalPersonality.aggressive:
