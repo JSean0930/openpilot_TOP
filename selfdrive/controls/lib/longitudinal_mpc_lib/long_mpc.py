78c6f52ca8614d128ed1497930070304d3b0c9c8
JSean0930 <122076254+JSean0930@users.noreply.github.com>
2025-06-18 23:45:37 +0800

    Update long_mpc.py

--- selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
+++ selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
@@ -169,7 +169,7 @@ def desired_follow_distance(v_ego, v_lead, t_follow=None, stop_distance=None):
     t_follow = get_T_FOLLOW()
   if stop_distance is None:
     stop_distance = get_STOP_DISTANCE()
-  return get_safe_obstacle_distance(v_ego, t_follow, stop_distance) - get_stopped_equivalence_factor(v_lead, v_ego)
+  return max(get_safe_obstacle_distance(v_ego, t_follow, stop_distance) - get_stopped_equivalence_factor(v_lead, v_ego), 0.0)
 
 
 def gen_long_model():
