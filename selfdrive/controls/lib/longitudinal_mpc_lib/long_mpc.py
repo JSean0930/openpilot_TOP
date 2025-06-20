dc2ea1817b6d89e8fc80bf236dda50d4ce1f4c65
JSean0930 <122076254+JSean0930@users.noreply.github.com>
2025-06-20 16:47:27 +0800

    Update long_mpc.py

--- selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
+++ selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
@@ -425,7 +425,7 @@ class LongitudinalMpc:
         sensitivity_gain = 4.0 # 起步靈敏
       else:
         sensitivity_gain = 3.0 # 煞車靈敏
-        a_lead_traj = a_lead * np.exp(-sensitivity_gain * a_lead_tau * (T_IDXS**2) / 2.)
+      a_lead_traj = a_lead * np.exp(-sensitivity_gain * a_lead_tau * (T_IDXS**2) / 2.)
     # 壅塞狀態（低速密集跟車）
     elif v_ego < 10.0:
       sensitivity_gain = 2.0
