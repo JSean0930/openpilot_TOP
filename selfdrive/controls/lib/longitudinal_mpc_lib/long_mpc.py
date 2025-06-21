81b16ac8df713c8d1e84f3b97f85abf887d31dad
JSean0930 <122076254+JSean0930@users.noreply.github.com>
2025-06-21 18:40:01 +0800

    更新 long_mpc.py

--- selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
+++ selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
@@ -137,9 +137,9 @@ def get_STOP_DISTANCE(personality=log.LongitudinalPersonality.standard):
   if personality==log.LongitudinalPersonality.relaxed:
     return 10.0
   elif personality==log.LongitudinalPersonality.standard:
-    return 6.0
+    return 11.0
   elif personality==log.LongitudinalPersonality.aggressive:
-    return 6.0
+    return 12.0
   else:
     raise NotImplementedError("Longitudinal personality not supported")
 
