890081a733112ce59826193065916b34d8452ae9
JSean0930 <122076254+JSean0930@users.noreply.github.com>
2025-07-02 20:01:27 +0800

    Update long_mpc.py

--- selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
+++ selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
@@ -71,7 +71,7 @@ def get_danger_zone_cost(v_ego):
   elif v_ego < 19.5:
     return 250.0#np.interp(v_ego, [10.0, 19.5], [130.0, 300.0])
   else:
-    return 200.0#np.interp(v_ego, [19.5, 27.8], [300.0, 600.0])
+    return 300.0#np.interp(v_ego, [19.5, 27.8], [300.0, 600.0])
 
 #def get_lead_danger_factor(v_ego):
   #return np.interp(v_ego, [0.0, 33.3], [1.0, 1.4])  # 線性插值，隨速度提升危險因子增加
