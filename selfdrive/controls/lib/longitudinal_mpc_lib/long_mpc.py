8fd4b81ef4dc18afa93d0aa16a0c4e90ad1a898b
JSean0930 <122076254+JSean0930@users.noreply.github.com>
2025-07-03 12:18:13 +0800

    Update long_mpc.py

--- selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
+++ selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
@@ -507,7 +507,7 @@ class LongitudinalMpc:
         self.set_weights(prev_accel_constraint=True, personality=personality, v_lead0=a_lead0, v_lead1=a_lead1)
     #==================================================================
     if self.mode == 'blended' and v_ego > high_thr:
-        
+        self.mode = 'acc'
 
     #==================================================================
     # 更新 prev_v_ego，供下一次使用
