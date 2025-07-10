496420ed1a9e95b6f96c6fe0adb712227056b8bb
JSean0930 <122076254+JSean0930@users.noreply.github.com>
2025-07-10 08:06:31 +0800

    Update long_mpc.py

--- selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
+++ selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
@@ -75,7 +75,7 @@ def get_danger_zone_cost(v_ego):
   # 線性插值：0 m/s → 100，33.3 m/s (120 km/h) → 300
   #return np.interp(v_ego, [0.0, 27.78], [120.0, 500.0])
   if v_ego <= mid_thr:
-    return 250.0
+    return 350.0
   elif v_ego <= high_thr:
     return 300.0#np.interp(v_ego, [10.0, 19.5], [130.0, 300.0])
   else:
